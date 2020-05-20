be19aafd65ae9538a136ac435db113a522979f75
yuesi <yue.sin@qq.com>
2020-05-20 17:00:37 +0800

    Init

---
+++ src/main/java/com/mybatis/generator/dao/SysUserMapper.java
@@ -0,0 +1,16 @@
+package com.mybatis.generator.dao;
+
+import com.mybatis.generator.model.SysUser;
+import java.util.List;
+
+public interface SysUserMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SysUser record);
+
+    SysUser selectByPrimaryKey(Integer id);
+
+    List<SysUser> selectAll();
+
+    int updateByPrimaryKey(SysUser record);
+}
---
+++ src/main/java/com/mybatis/generator/model/SysUser.java
@@ -0,0 +1,53 @@
+package com.mybatis.generator.model;
+
+public class SysUser {
+    private Integer id;
+
+    private String name;
+
+    private Integer age;
+
+    private Integer sex;
+
+    private String address;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Integer getSex() {
+        return sex;
+    }
+
+    public void setSex(Integer sex) {
+        this.sex = sex;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address == null ? null : address.trim();
+    }
+}
